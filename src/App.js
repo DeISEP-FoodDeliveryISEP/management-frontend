14bd4374e509d6f35abd7a57e97b7051d515a254
Andy Chang <andy.rochi@gmail.com>
2023-05-11 14:40:41 +0200

    feat: configure pages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import './App.css';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Login from "./pages/Login.js";
+import Home from "./pages/Home.js";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
     <div>
-      <h1>Basic Example</h1>
-
-      <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </p>
-
       {/* Routes nest inside one another. Nested route paths build upon
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
 
           {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
           <Route path="*" element={<NoMatch />} />
         </Route>
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
@@ -43,15 +35,6 @@ function Layout() {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
         </ul>
       </nav>
 
@@ -64,41 +47,5 @@ function Layout() {
     </div>
   );
 }
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
-}
-
 
 export default App;
