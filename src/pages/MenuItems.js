66d3f3a3d250a798527e0df9867bd3b2d5b6333e
Andy Chang <andy.rochi@gmail.com>
2023-05-15 14:21:48 +0200

    layout: complete menu-items management layout

--- src/pages/MenuItems.js
+++ src/pages/MenuItems.js
@@ -1,5 +1,18 @@
+import "../assets/menu-items.css";
+import MenuItemTable from "../components/MenuItemsTable";
+import { ButtonGroup, SHAPE } from "baseui/button-group";
+import { Button } from "baseui/button";
+
 export default function MenuItems() {
   return (
-    <></>
+    <div className="menu-items-container">
+        <h1>Menu Items Management</h1>
+        <ButtonGroup shape={SHAPE.pill}>
+        <Button>+ New</Button>
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+        </ButtonGroup>
+        <MenuItemTable />
+    </div>
   );
 }
