8a483885291643ced7dca428e9711c453419a3e8
Andy Chang <andy.rochi@gmail.com>
2023-06-12 00:06:29 +0200

    feat: set navbar username

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,45 +1,60 @@
 import * as React from "react";
 import '../assets/navbar.css';
 import {
-  AppNavBar,
-  setItemActive
+  AppNavBar
 } from "baseui/app-nav-bar";
-import {
-  ChevronDown,
-  Delete,
-  Overflow,
-  Upload
-} from "baseui/icon";
 import { Link } from "react-router-dom";
+import { HiOutlineLogout } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import { logoutApi } from "../api/login";
+import { toaster } from "baseui/toast";
 
 export const NavBar =  () => {
-  const [mainItems, setMainItems] = React.useState([
-    { icon: Upload, label: "Main A" },
-    {
-      active: true,
-      icon: ChevronDown,
-      label: "Main B",
-      navExitIcon: Delete,
-      children: [
-        { icon: Upload, label: "Secondary A" },
-        { icon: Upload, label: "Secondary B" }
-      ]
+  
+  const [userInfo, setUserInfo] = React.useState({});
+  const [username, setUsername] = React.useState("");
+  const [name, setName] = React.useState("");
+  const navigate = useNavigate();
+
+  React.useEffect(()=>{
+    const curUserInfo = JSON.parse(localStorage.getItem('userInfo'));
+    setUserInfo(curUserInfo);
+    if (curUserInfo !== null) {
+      setName(curUserInfo['name']);
+      setUsername(curUserInfo['username']);
     }
-  ]);
+    else {
+      toaster.warning("*local* User not login, redirect");
+      navigate("/login");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+
+  async function handleLogout() {
+        let res = await logoutApi();
+        if (String(res.code) === '1') {// login success
+            localStorage.removeItem('userInfo');
+            navigate("/login");
+        } else {
+            toaster.warning(res.msg);
+        }
+    }
+
   return (
     <AppNavBar
       title={<Link to="/" className="logo-link"><div ><span style={{color: "#FFD643"}}>De</span>ISEP | Management System</div></Link>}
-      // mainItems={mainItems}
-      // onMainItemSelect={item => {
-      //   setMainItems(prev => setItemActive(prev, item));
-      // }}
-      username="Thomas Wang"
-      usernameSubtitle="5 Stars"
+      username={name === "管理员" ? "Administrator" : name}
+      usernameSubtitle={username}
       userItems={[
-        { icon: Overflow, label: "User A" },
-        { icon: Overflow, label: "User B" }
+        { icon: HiOutlineLogout, label: "Logout" }
       ]}
-      onUserItemSelect={item => console.log(item)}
+      onUserItemSelect={item => {
+        if (item.label === "Logout") {
+          handleLogout();
+        }
+      }}
     />
   );
 }
